a6a0cbfd9d78c0636d06d1b21d1579c5b9893f8a
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-25 13:16:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// setup default values
+let day = 0
+let age = 0
+// event listener
+document.getElementById('button').addEventListener('click', calculate)
+// main function
+function calculate () {
+  day = document.getElementById('day').value
+  day = parseInt(day)
+  age = document.getElementById('age').value
+  age = parseInt(age)
+  if (age < 18 && day > 2){
+    alert('Time for school!')
+  }  else if (age >= 18 && day > 2){
+    alert('Time to go to work!')
+  } else {
+    alert('Time to relax for the weekend!')
+  }
+}
