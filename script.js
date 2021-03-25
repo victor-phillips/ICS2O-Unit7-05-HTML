594030041bf279c2e1f5d98eb3cf402988a20b47
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-25 13:21:40 +0000

    fixed spaces

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function calculate () {
   day = parseInt(day)
   age = document.getElementById('age').value
   age = parseInt(age)
-  if (age < 18 && day > 2){
+  if (age < 18 && day > 2) {
     alert('Time for school!')
-  }  else if (age >= 18 && day > 2){
+  } else if (age >= 18 && day > 2) {
     alert('Time to go to work!')
   } else {
     alert('Time to relax for the weekend!')
